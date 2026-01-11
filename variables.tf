afe48fa9a892f9047259fbe8b96c2f9da0b80a8f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:51:03 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,108 @@
+variable "netapp_volume_group_oracles" {
+  description = <<EOT
+Map of netapp_volume_group_oracles, attributes below
+Required:
+    - account_name
+    - application_identifier
+    - group_description
+    - location
+    - name
+    - resource_group_name
+    - volume (block):
+        - capacity_pool_id (required)
+        - data_protection_replication (optional, block):
+            - endpoint_type (optional)
+            - remote_volume_location (required)
+            - remote_volume_resource_id (required)
+            - replication_frequency (required)
+        - data_protection_snapshot_policy (optional, block):
+            - snapshot_policy_id (required)
+        - encryption_key_source (optional)
+        - export_policy_rule (required, block):
+            - allowed_clients (required)
+            - nfsv3_enabled (required)
+            - nfsv41_enabled (required)
+            - root_access_enabled (optional)
+            - rule_index (required)
+            - unix_read_only (optional)
+            - unix_read_write (optional)
+        - key_vault_private_endpoint_id (optional)
+        - name (required)
+        - network_features (optional)
+        - protocols (required)
+        - proximity_placement_group_id (optional)
+        - security_style (required)
+        - service_level (required)
+        - snapshot_directory_visible (required)
+        - storage_quota_in_gb (required)
+        - subnet_id (required)
+        - tags (optional)
+        - throughput_in_mibps (required)
+        - volume_path (required)
+        - volume_spec_name (required)
+        - zone (optional)
+EOT
+
+  type = map(object({
+    account_name           = string
+    application_identifier = string
+    group_description      = string
+    location               = string
+    name                   = string
+    resource_group_name    = string
+    volume = list(object({
+      capacity_pool_id = string
+      data_protection_replication = optional(object({
+        endpoint_type             = optional(string, "dst")
+        remote_volume_location    = string
+        remote_volume_resource_id = string
+        replication_frequency     = string
+      }))
+      data_protection_snapshot_policy = optional(object({
+        snapshot_policy_id = string
+      }))
+      encryption_key_source = optional(string)
+      export_policy_rule = list(object({
+        allowed_clients     = string
+        nfsv3_enabled       = bool
+        nfsv41_enabled      = bool
+        root_access_enabled = optional(bool, true)
+        rule_index          = number
+        unix_read_only      = optional(bool, false)
+        unix_read_write     = optional(bool, true)
+      }))
+      key_vault_private_endpoint_id = optional(string)
+      name                          = string
+      network_features              = optional(string)
+      protocols                     = list(string)
+      proximity_placement_group_id  = optional(string)
+      security_style                = string
+      service_level                 = string
+      snapshot_directory_visible    = bool
+      storage_quota_in_gb           = number
+      subnet_id                     = string
+      tags                          = optional(map(string))
+      throughput_in_mibps           = number
+      volume_path                   = string
+      volume_spec_name              = string
+      zone                          = optional(string)
+    }))
+  }))
+  validation {
+    condition = alltrue([
+      for k, v in var.netapp_volume_group_oracles : (
+        length(v.volume) >= 2 && length(v.volume) <= 12
+      )
+    ])
+    error_message = "Each volume list must contain between 2 and 12 items"
+  }
+  validation {
+    condition = alltrue([
+      for k, v in var.netapp_volume_group_oracles : (
+        alltrue([for item in v.volume : (length(item.export_policy_rule) >= 1 && length(item.export_policy_rule) <= 5)])
+      )
+    ])
+    error_message = "Each export_policy_rule list must contain between 1 and 5 items"
+  }
+}
+
