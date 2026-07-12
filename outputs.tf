1d8e3d10574db7e351df3de96e9a9d6992fc537c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "netapp_volume_group_oracles" {
-  description = "All netapp_volume_group_oracle resources"
-  value       = azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles
-}
 output "netapp_volume_group_oracles_account_name" {
-  description = "List of account_name values across all netapp_volume_group_oracles"
-  value       = [for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : v.account_name]
+  description = "Map of account_name values across all netapp_volume_group_oracles, keyed the same as var.netapp_volume_group_oracles"
+  value       = { for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : k => v.account_name }
 }
 output "netapp_volume_group_oracles_application_identifier" {
-  description = "List of application_identifier values across all netapp_volume_group_oracles"
-  value       = [for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : v.application_identifier]
+  description = "Map of application_identifier values across all netapp_volume_group_oracles, keyed the same as var.netapp_volume_group_oracles"
+  value       = { for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : k => v.application_identifier }
 }
 output "netapp_volume_group_oracles_group_description" {
-  description = "List of group_description values across all netapp_volume_group_oracles"
-  value       = [for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : v.group_description]
+  description = "Map of group_description values across all netapp_volume_group_oracles, keyed the same as var.netapp_volume_group_oracles"
+  value       = { for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : k => v.group_description }
 }
 output "netapp_volume_group_oracles_location" {
-  description = "List of location values across all netapp_volume_group_oracles"
-  value       = [for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : v.location]
+  description = "Map of location values across all netapp_volume_group_oracles, keyed the same as var.netapp_volume_group_oracles"
+  value       = { for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : k => v.location }
 }
 output "netapp_volume_group_oracles_name" {
-  description = "List of name values across all netapp_volume_group_oracles"
-  value       = [for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : v.name]
+  description = "Map of name values across all netapp_volume_group_oracles, keyed the same as var.netapp_volume_group_oracles"
+  value       = { for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : k => v.name }
 }
 output "netapp_volume_group_oracles_resource_group_name" {
-  description = "List of resource_group_name values across all netapp_volume_group_oracles"
-  value       = [for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : v.resource_group_name]
+  description = "Map of resource_group_name values across all netapp_volume_group_oracles, keyed the same as var.netapp_volume_group_oracles"
+  value       = { for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : k => v.resource_group_name }
 }
 output "netapp_volume_group_oracles_volume" {
-  description = "List of volume values across all netapp_volume_group_oracles"
-  value       = [for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : v.volume]
+  description = "Map of volume values across all netapp_volume_group_oracles, keyed the same as var.netapp_volume_group_oracles"
+  value       = { for k, v in azurerm_netapp_volume_group_oracle.netapp_volume_group_oracles : k => v.volume }
 }
 
